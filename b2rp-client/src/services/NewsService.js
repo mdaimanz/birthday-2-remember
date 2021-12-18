60881ccec6bb1b34be741dbfa48171af76f0f455
mdaimanz <39907470+mdaimanz@users.noreply.github.com>
2021-12-19 04:59:41 +0800

    Added API entry point for News Module
    - getNews()
    - getNewsContent()
    - addNews()
    - deleteNews()
    - editNews()

--- b2rp-client/src/services/NewsService.js
+++ b2rp-client/src/services/NewsService.js
@@ -6,6 +6,16 @@ export default{
     },
     getNewsContent(news_id){
         return Api().get('getNewsContent/'+news_id)
+    },
+    addNews(news){
+        return Api().post('addNews', news)
+    },
+    deleteNews(news_id){
+        return Api().post('deleteNews/'+news_id)
+    },
+    editNews(news_id,news){
+        return Api().post('editNews/'+news_id, news)
     }
+
 
 }
