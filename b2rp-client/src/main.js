1ad1dcadcd7f33cba0820b7fdd9b171c0a6a8b6b
mdaimanz <39907470+mdaimanz@users.noreply.github.com>
2021-12-19 15:34:18 +0800

    Added Vue to use Vuex.Store

--- b2rp-client/src/main.js
+++ b2rp-client/src/main.js
@@ -4,13 +4,17 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import { sync } from 'vuex-router-sync'
+import store from '@/store/store'
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios)
 
+sync(store, router)
 
 new Vue({
   router,
   vuetify,
+  store,
   render: h => h(App)
 }).$mount('#app')
