16caca87de85e92d11d745284de30cf6aa250a4e
mdaimanz <39907470+mdaimanz@users.noreply.github.com>
2021-12-19 12:58:07 +0800

    Added all Reminder module method

---
+++ b2rp-client/src/services/ReminderService.js
@@ -0,0 +1,25 @@
+import Api from '@/services/Api'
+
+export default{
+    getAllReminder(user_id){
+        return Api().get('getAllReminder/'+user_id)
+    },
+
+    deleteReminder(user_id, remind_id){
+        return Api().post('deleteReminder/'+user_id+'/'+remind_id)
+    },
+
+    getReminder(remind_id){
+        return Api().get('getReminder/'+remind_id)
+    },
+
+    editReminder(user_id, remind_id, reminder){
+        return Api().post('editReminder/'+user_id+'/'+remind_id, reminder)
+    },
+
+    addReminder(user_id, reminder){
+        return Api().post('addReminder/'+user_id, reminder)
+    }
+
+
+}
